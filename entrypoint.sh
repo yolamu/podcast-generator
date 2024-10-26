47a793e55cac9e65ca8e271c5a84d418b5eb0363
yolamu <32828093+yolamu@users.noreply.github.com>
2024-10-26 18:47:58 +0000

    gen files

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "===================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
+
+echo "===================="
